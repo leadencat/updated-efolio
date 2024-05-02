a7b4543918ab5929b465d5447c07a2454cf20ad6
leadencat <jdanders1999@gmail.com>
2024-05-02 17:01:13 -0400

    updated README

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,14 @@
 export default function Projects() {
-    return (
-      <div>
-        <header1>Projects Page</header1>
-        <p>
-             cards here
-        </p>
+  return (
+    <div className="container-fluid text-center pt-5">
+      <div className="row">
+        <div className="col-lg-6 col-md-12 mb-4">
+          <div className="card">
+            <header1>Projects Page</header1>
+            <div className="card-body"></div>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
