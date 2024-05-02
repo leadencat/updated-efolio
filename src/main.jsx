0c676d0e806079e3c174dc30f483c1f2bb6d0623
leadencat <jdanders1999@gmail.com>
2024-05-02 16:37:36 -0400

    NavTabs for beginning of page display

---
+++ src/main.jsx
@@ -0,0 +1,48 @@
+import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/AboutMe',
+        element: <AboutMe />,
+      },
+      {
+        path: '/Projects',
+        element: <Projects />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
